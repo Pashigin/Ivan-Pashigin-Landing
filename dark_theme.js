6969cc9d768cefbd90159cc319ba226448747ec6
Pashigin <ivanp2@yandex.ru>
2024-11-26 01:17:45 +0400

    code optimisation

--- dark_theme.js
+++ dark_theme.js
@@ -14,19 +14,21 @@ function applyTheme(theme) {
     const contactLinks = document.querySelectorAll('#contact a');
     const sendButton = document.querySelector('#contact button[type="submit"]');
     const portfolioImages = document.querySelectorAll('#portfolio img');
+    const profileImage = document.getElementById('profileImage');
 
     if (theme === 'dark') {
         emailButton.classList.replace('btn-outline-dark', 'btn-outline-light');
         githubButton.classList.replace('btn-outline-dark', 'btn-outline-light');
         contactLinks.forEach(link => link.classList.replace('link-dark', 'link-light'));
         sendButton.classList.replace('btn-dark', 'btn-outline-light');
-        portfolioImages.forEach(image => image.style.filter = 'grayscale(100%) brightness(0.9)');
+        profileImage.style.filter = 'brightness(0.8)';
     } else {
         emailButton.classList.replace('btn-outline-light', 'btn-outline-dark');
         githubButton.classList.replace('btn-outline-light', 'btn-outline-dark');
         contactLinks.forEach(link => link.classList.replace('link-light', 'link-dark'));
         sendButton.classList.replace('btn-outline-light', 'btn-dark');
         portfolioImages.forEach(image => image.style.filter = 'none');
+        profileImage.style.filter = 'brightness(1)';
     }
 }
 
@@ -36,18 +38,13 @@ function detectSystemTheme() {
 }
 
 // Check for saved theme on page load
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-    applyTheme(savedTheme);
-} else {
-    const systemTheme = detectSystemTheme();
-    applyTheme(systemTheme); // Default to system theme
-}
+const savedTheme = localStorage.getItem('theme') || detectSystemTheme();
+applyTheme(savedTheme);
+
 
 // Theme toggle event
 themeToggle.addEventListener('click', () => {
-    const currentTheme = root.getAttribute('data-bs-theme');
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    const newTheme = root.getAttribute('data-bs-theme') === 'light' ? 'dark' : 'light';
     applyTheme(newTheme);
     localStorage.setItem('theme', newTheme);
 });
